08259523ffa3226bd32080830f9b72ae228624fe
Alexey Rusel <alexeyrusel@gmail.com>
2023-04-18 19:48:27 +0300

    fix: fix body height problem

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -3,9 +3,13 @@ import { Outlet } from "react-router-dom";
 
 import { Header } from "../header/header";
 
+import styles from "./layout.module.scss";
+
 export const Layout = () => (
-    <>
+    <div className={styles.wrapper}>
         <Header />
-        <Outlet />
-    </>
+        <main className={styles.main}>
+            <Outlet />
+        </main>
+    </div>
 );
